09276058da4251d529fb9fbeebcba31fa6b13e51
Matt Freeman <mgregfreeman@gmail.com>
2016-01-12 22:48:23 -0700

    updated user privileges for commands in rails

--- rails.sh
+++ rails.sh
@@ -2,12 +2,15 @@
 yum install sqlite-devel -y
 yum install -y gcc libxml2 libxml2-devel libxslt libxslt-devel
 yum install gcc-c++ -y
+yum install ruby-devel -y
+yum install ruby-irb -y
+su ec2-user
 # download and untar rubygems
 wget https://rubygems.org/rubygems/rubygems-2.5.1.tgz
 tar -xzf rubygems-2.5.1.tgz
-ruby rubygems-2.5.1/setup.rb
-yum install ruby-devel -y
-yum install ruby-irb -y
+cd rubygems-2.5.1
+ruby setup.rb
+cd ..
 gem install bundler
 gem install io-console
 git clone https://github.com/cvhooper22/462_lab0.git
